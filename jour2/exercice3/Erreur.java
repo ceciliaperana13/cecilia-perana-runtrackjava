01d622e16cc656b239325e038623936d36a876c1
ceciliaperana13 <cperana13@gmail.com>
2026-03-31 15:00:51 +0200

    exercice 3 en cours

---
+++ jour2/exercice3/Erreur.java
@@ -0,0 +1,16 @@
+package jour2.exercice3;
+class Erreur {
+    
+    Public static void main(String[] args) {
+        E1 x = new E1();
+        E2 y = new E2();
+        E3 z = new E3();
+        E4 v = new E4();
+        E5 w = new E5();
+        System.out.println(x.a);
+        System.out.println(y.c);
+        System.out.println(z);
+        System.out.println(v);
+        System.out.println(w);
+    }
+}
