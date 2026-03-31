483cf70c48a53bbb28f234d565841bc4045ba84b
ceciliaperana13 <cperana13@gmail.com>
2026-03-31 08:32:10 +0200

    job11 fais

--- jour1/exercice10/job10.java
+++ jour1/exercice10/job10.java
@@ -1,5 +1,14 @@
 package jour1.exercice10;
 
 public class job10 {
-    
+    //calcule la factorielle de 8.
+    public static void main(String[] args) {
+        int n = 8;
+        long factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        System.out.println("La factorielle de " + n + " est: " + factorial);
+    }
+
 }
